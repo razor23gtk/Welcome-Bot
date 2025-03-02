a489c2adba7f5973f86a7af86f1d94266279d781
razor23gtk <razorclaw996@gmail.com>
2025-03-02 03:02:58 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     token : "",
-    mongoURI : "",
+    mongoURI : "mongodb+srv://razorclaw996:0654788685razor@discordbot.dkbsd.mongodb.net/?retryWrites=true&w=majority
+",
     statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
     statusContent: "A", // A = Show total users count, B = Show total servers count, C = Custom message
     customStatus: "GlaceYT",
