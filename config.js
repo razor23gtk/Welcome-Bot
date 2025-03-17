91fc16d58f494dd0de44d61f5a0ffe4b78e67f9b
razor23gtk <razorclaw996@gmail.com>
2025-03-17 00:08:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 module.exports = {
     token : "",
-    mongoURI : "mongodb+srv://razorclaw996:0654788685razor@discordbot.dkbsd.mongodb.net/?retryWrites=true&w=majority
-",
+    mongoURI : "mongodb+srv://razorclaw996:0654788685razor@discordbot.dkbsd.mongodb.net/?retryWrites=true&w=majority",
     statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
     statusContent: "A", // A = Show total users count, B = Show total servers count, C = Custom message
-    customStatus: "GlaceYT",
-    streamingURL: "https://www.twitch.tv/glaceytt"
+    customStatus: "WisdomGuard",
+    streamingURL: "https://discord.gg/QGMKQd9NJW"
 };
